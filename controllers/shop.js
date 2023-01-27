80dad24ff036e9a1f0720242707d18dc1f79a7ec
Aditya <aditya18.mail@gmail.com>
2023-01-27 13:53:26 +0530

    Implementing Order System

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const { populate } = require('../models/user');
+const Order = require('../models/order')
 
 
 exports.getProducts = (req, res, next) => {
@@ -90,25 +90,43 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-// exports.postOrder = (req, res, next) => {
-//   let fetchedCart;
-//   req.user
-//     .addOrder()
-//     .then(result => {
-//       res.redirect('/orders');
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.postOrder = (req, res, next) => {
+ 
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      // console.log(user.cart.items)
+      const products = user.cart.items.map(i=> {
+        return {quantity : i.quantity , product : { ...i.productId._doc}}
+      })
+      const order = new Order({
+        user:{
+          name:req.user.name,
+          userId:req.user
+        },
+        products: products
+      })
+      return order.save() ;
+      
+    })
+    .then(result => {
+      return req.user.clearCart()
+    })
+    .then(()=>{
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+};
 
-// exports.getOrders = (req, res, next) => {
-//   req.user
-//     .getOrders()
-//     .then(orders => {
-//       res.render('shop/orders', {
-//         path: '/orders',
-//         pageTitle: 'Your Orders',
-//         orders: orders
-//       });
-//     })
-//     .catch(err => console.log(err));
-// };
+
+exports.getOrders = (req, res, next) => {
+  Order.find({'user.userId':req.user._id})
+  .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
+};
